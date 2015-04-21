70384521b424d6163dcb922cee588eaf3dc928d3
Hui Wu <huisen.wu@gmail.com>
2015-04-21 10:14:27 -0500

    optimize code

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var loaderUtils = require("loader-utils");
 
 module.exports = function(source) {
-	if(this.cacheable) {
-		this.cacheable();
-	}
+	if(this.cacheable) this.cacheable();
 
 	var query = loaderUtils.parseQuery(this.query);
 	var prefix = query.prefix || '';
@@ -11,19 +9,11 @@ module.exports = function(source) {
 	var react = query.react || '';
 	var ngReact = query.ngReact || '';
 
-	console.log('prefix test: ' + prefix);
-	console.log('angular test: ' + angular);
-	console.log('react test: ' + react);
-	console.log('ngReact test: ' + ngReact);
-
 	var conf = JSON.parse(source);
 	var keys = Object.keys(conf);
 	var deps = keys.map(function(key) {
 		return "'" + conf[key] + "'";
 	}).join(",");
-
-	console.log('keys test: ' + keys);
-	console.log('deps test: ' + deps);
 
 	var directives = keys.map(function(key) {
 		return ".value('"+ key + "', " + key + ").directive('" + prefix + key + "', ['reactDirective', function(reactDirective) {return reactDirective('" + key + "', []);}])";
