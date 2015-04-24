9f955a477ff04f680aa16aea70155578bf25b03b
Wu, Hui <Hui.Wu@charter.com>
2015-04-24 18:07:39 -0500

    Modified directive-laoder

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 var loaderUtils = require("loader-utils");
+var _ = require('lodash');
 
 module.exports = function(source) {
 	if(this.cacheable) this.cacheable();
 
 	var query = loaderUtils.parseQuery(this.query);
-	var prefix = query.prefix || '';
-	var angular = query.angular || '';
-	var react = query.react || '';
-	var ngReact = query.ngReact || '';
+	var prefix = query.prefix;
 
-	var conf = JSON.parse(source);
-	var keys = Object.keys(conf);
-	var deps = keys.map(function(key) {
-		return "'" + conf[key] + "'";
-	}).join(",");
+	var templates = {
+		component: "<%=key%>:{Componenet:<%=key%>,render:function(props, target) {React.render(React.createElement(<%=key%>, props), target);},registerDirective: function(app, name) {app.value('<%=key%>Component', Test).directive('<%=prefix%>'+name, ['reactDirective', function(reactDirective) {return reactDirective('<%=key%>Component');}]);}}",
+		amd: "define([<%=values%>,'<%=react%>'], function(<%=keys%>,React) {return {<%=comps%>};});"
+	};
 
-	var directives = keys.map(function(key) {
-		return ".value('"+ key + "', " + key + ").directive('" + prefix + key + "', ['reactDirective', function(reactDirective) {return reactDirective('" + key + "');}])";
-	}).join("");
+	var conf = _.mapValues(JSON.parse(source), function(value) {
+		return "'" + value + "'";
+	});
 
-	return "define(["+ deps +",'" + react + "','"+ angular +"','"+ ngReact +"'], function("+ keys +",React,angular) {" + "angular.module('"+ prefix + "Application', ['react'])" + directives + ";" + "});";
+	var keys = _.keys(conf);
+
+	var comp = _.template(templates.component);
+	var comps = _.map(keys, function(key) {
+		return comp({key: key, prefix: query.prefix});
+	});
+
+	console.log(comps);
+
+	var amd = _.template(templates.amd);
+
+	var result = amd({values: _.values(conf), react: query.react, keys: keys, comps: comps});
+
+	console.log(result);
+
+	return result;
 };
