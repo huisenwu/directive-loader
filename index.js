160ae664a3942f52cabfeeec06af4f65850dfa92
Hui Wu <huisen.wu@gmail.com>
2015-04-25 15:50:25 -0500

    optimize the code

--- index.js
+++ index.js
@@ -5,31 +5,17 @@ module.exports = function(source) {
 	if(this.cacheable) this.cacheable();
 
 	var query = loaderUtils.parseQuery(this.query);
-	var prefix = query.prefix;
-
 	var templates = {
 		component: "<%=key%>:{Componenet:<%=key%>,render:function(props, target) {React.render(React.createElement(<%=key%>, props), target);},registerDirective: function(app, name) {app.value('<%=key%>Component', Test).directive('<%=prefix%>'+name, ['reactDirective', function(reactDirective) {return reactDirective('<%=key%>Component');}]);}}",
 		amd: "define([<%=values%>,'<%=react%>'], function(<%=keys%>,React) {return {<%=comps%>};});"
 	};
-
-	var conf = _.mapValues(JSON.parse(source), function(value) {
-		return "'" + value + "'";
-	});
-
+	var conf = JSON.parse(source);
 	var keys = _.keys(conf);
-
 	var comp = _.template(templates.component);
 	var comps = _.map(keys, function(key) {
 		return comp({key: key, prefix: query.prefix});
 	});
-
-	console.log(comps);
-
 	var amd = _.template(templates.amd);
-
-	var result = amd({values: _.values(conf), react: query.react, keys: keys, comps: comps});
-
-	console.log(result);
-
+	var result = amd({values: _.map(_.values(conf), function(value) {return "'" + value + "'";}), react: query.react, keys: keys, comps: comps});
 	return result;
 };
